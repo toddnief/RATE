9abf1308e99cde29d4baf72b4ee72cdf5bff8de2
Todd Nief <tnief@uchicago.edu>
2024-11-19 18:30:03 -0600

    rewrite treatment effects to remove old code

--- src/rate/treatment_effects.py
+++ src/rate/treatment_effects.py
@@ -2,41 +2,18 @@
 
 import argparse
 import pprint
-from typing import Any, Dict, Tuple
+from typing import Any, Dict
 
 import numpy as np
 import yaml
-from constants import (  # noqa
+from constants import (
     EFFECTS_DIR,
     FILE_ID,
-    REWRITES_DATASET_NAME,
-    REWRITES_DIR,
     ROOT_DIR,
     SCORED_DIR,
     logging,
 )
 from utils import load_dataset_from_json, write_to_json
-
-
-def calculate_unpaired_treatment_effect(
-    treated: np.ndarray, untreated: np.ndarray
-) -> Tuple[float, float]:
-    """
-    Calculates the treatment effect and standard error for unpaired data.
-
-    Args:
-        treated: An array of outcomes for the treated group.
-        untreated: An array of outcomes for the untreated group.
-
-    Returns:
-        A tuple containing the treatment effect (difference in means) and the standard error of the difference.
-    """
-    treated_effect = np.mean(treated) - np.mean(untreated)
-    stderr = np.sqrt(
-        np.var(treated, ddof=1) / len(treated)
-        + np.var(untreated, ddof=1) / len(untreated)
-    )
-    return treated_effect, stderr
 
 
 def calculate_rewrite_effect(
@@ -87,97 +64,34 @@ def calculate_rewrite_effect(
     }
 
 
-def calculate_average_treatment_effects(
-    dataset: Dict[str, Any], rewritten_rewrites: bool = True, **effects_template
-) -> Dict[str, float]:
+def _calculate_treatment_effects(treated, untreated, paired=False):
     """
-    Calculate the average treatment effects (ATE, ATT, ATU) and their standard errors.
-
-    This function computes:
-    - ATE (Average Treatment Effect)
-    - ATT (Average Treatment Effect on the Treated)
-    - ATU (Average Treatment Effect on the Untreated)
-    - A naive effect
-    It uses both paired and unpaired treatment effect calculations, with standard errors for each estimate.
+    Calculates the treatment effect and standard error for unpaired data.
 
     Args:
-        dataset: A dictionary where each key is an identifier, and the value is another dictionary
-                 containing outcome data for different reward models (keys for "original" and "rewrite" outcomes).
-        **effects_template: Optional arguments that specify the reward model's keys.
-                            Default keys:
-                            - reward_key: "reward"
-                            - original: "rewritten rewrite"
-                            - rewrite: "rewrite"
+        treated: An array of outcomes for the treated group.
+        untreated: An array of outcomes for the untreated group.
 
     Returns:
-        A dictionary containing treatment effects and standard errors
+        A tuple containing the treatment effect (difference in means) and the standard error of the difference.
     """
-    reward_key = effects_template.get("reward_key", "reward")
+    if not isinstance(treated, np.ndarray):
+        treated = np.array(treated)
+    if not isinstance(untreated, np.ndarray):
+        untreated = np.array(untreated)
 
-    if rewritten_rewrites:
-        w_original_key = effects_template.get(
-            "w_rewritten_rewrite_key", "rewritten rewrite"
+    treated_effect = np.mean(treated) - np.mean(untreated)
+    if not paired:
+        stderr = np.sqrt(
+            np.var(treated, ddof=1) / len(treated)
+            + np.var(untreated, ddof=1) / len(untreated)
         )
     else:
-        w_original_key = effects_template.get("w_original_key", "original")
-    w_counterfactual_key = effects_template.get("w_counterfactual_key", "rewrite")
-
-    do_w_1 = []
-    do_w_0 = []
-    w_1 = []
-    w_0 = []
-    w_0_for_w_1 = []  # Used to calculate ATT & ATU
-    w_1_for_w_0 = []
-
-    ate_effects = []
-
-    for example in dataset.values():
-        if example["w_original"]:
-            do_w_1.append(example[reward_key][w_original_key])
-            do_w_0.append(example[reward_key][w_counterfactual_key])
-
-            w_1.append(example[reward_key][w_original_key])
-            w_0_for_w_1.append(example[reward_key][w_counterfactual_key])
-            ate_effects.append(
-                example[reward_key][w_original_key]
-                - example[reward_key][w_counterfactual_key]
-            )
-        else:
-            do_w_1.append(example[reward_key][w_counterfactual_key])
-            do_w_0.append(example[reward_key][w_original_key])
-
-            w_0.append(example[reward_key][w_original_key])
-            w_1_for_w_0.append(example[reward_key][w_counterfactual_key])
-            ate_effects.append(
-                example[reward_key][w_counterfactual_key]
-                - example[reward_key][w_original_key]
-            )
-
-    do_w_1, do_w_0 = np.asarray(do_w_1), np.asarray(do_w_0)
-    w_1, w_0 = np.asarray(w_1), np.asarray(w_0)
-    w_1_for_w_0, w_0_for_w_1 = np.asarray(w_1_for_w_0), np.asarray(w_0_for_w_1)
-
-    ATE, ATE_unpaired_stderr = calculate_unpaired_treatment_effect(do_w_1, do_w_0)
-    ATE_paired_stderr = np.std(np.asarray(ate_effects), ddof=1) / np.sqrt(
-        len(ate_effects)
-    )
-    ATT, ATT_stderr = calculate_unpaired_treatment_effect(w_1, w_0_for_w_1)
-    ATU, ATU_stderr = calculate_unpaired_treatment_effect(w_1_for_w_0, w_0)
-    naive_effect, naive_effect_stderr = calculate_unpaired_treatment_effect(w_1, w_0)
-
-    return {
-        "ATE": ATE,
-        "ATE_stderr": ATE_paired_stderr,
-        "ATT": ATT,
-        "ATT_stderr": ATT_stderr,
-        "ATU": ATU,
-        "ATU_stderr": ATU_stderr,
-        "naive_effect": naive_effect,
-        "naive_effect_stderr": naive_effect_stderr,
-    }
+        stderr = np.std(treated - untreated, ddof=1) / np.sqrt(len(treated))
+    return treated_effect, stderr
 
 
-def calculate_treatment_effects(
+def treatment_effects_pipeline(
     dataset: Dict[str, Any], **effects_template
 ) -> Dict[str, float]:
     """
@@ -192,16 +106,67 @@ def calculate_treatment_effects(
         A dictionary containing treatment effects and standard errors
     """
     w_original_key = effects_template.get("w_original_key", "original")
+    w_counterfactual_key = effects_template.get("w_counterfactual_key", "rewrite")
+    w_rewritten_rewrite_key = effects_template.get(
+        "w_rewritten_rewrite_key", "rewritten rewrite"
+    )
+    reward_key = effects_template.get("reward_key", "reward")
 
     Y1_count = 0
+    # Used for naive estimator
     Y1_rewards = []
     Y0_rewards = []
+    # Used for ATT and ATU
+    Y1_rewritten_rewards = []
+    Y0_rewritten_rewards = []
+    # Used for ATT and ATU with rewritten rewrites
+    Y1_rewritten_rewrite_rewards = []
+    Y0_rewritten_rewrite_rewards = []
+    # Used for ATE with single rewrite
+    Y_do1_rewards = []
+    Y_do0_rewards = []
+    # Used for ATE with rewritten rewrites
+    Y_do1_rewritten_rewrite_rewards = []
+    Y_do0_rewritten_rewrite_rewards = []
+
     for example in dataset.values():
         if example["w_original"]:
             Y1_count += 1
-            Y1_rewards.append(example[effects_template["reward_key"]][w_original_key])
+            Y1_rewards.append(example[reward_key][w_original_key])
+
+            # Single rewrite
+            Y_do1_rewards.append(example[reward_key][w_original_key])
+            Y_do0_rewards.append(example[reward_key][w_counterfactual_key])
+            Y0_rewritten_rewards.append(example[reward_key][w_counterfactual_key])
+
+            # Rewritten rewrites
+            Y_do1_rewritten_rewrite_rewards.append(
+                example[reward_key][w_rewritten_rewrite_key]
+            )
+            Y_do0_rewritten_rewrite_rewards.append(
+                example[reward_key][w_counterfactual_key]
+            )
+            Y1_rewritten_rewrite_rewards.append(
+                example[reward_key][w_rewritten_rewrite_key]
+            )
         else:
             Y0_rewards.append(example[effects_template["reward_key"]][w_original_key])
+
+            # Single rewrite
+            Y_do1_rewards.append(example[reward_key][w_counterfactual_key])
+            Y_do0_rewards.append(example[reward_key][w_original_key])
+            Y1_rewritten_rewards.append(example[reward_key][w_counterfactual_key])
+
+            # Rewritten rewrites
+            Y_do1_rewritten_rewrite_rewards.append(
+                example[reward_key][w_counterfactual_key]
+            )
+            Y_do0_rewritten_rewrite_rewards.append(
+                example[reward_key][w_rewritten_rewrite_key]
+            )
+            Y0_rewritten_rewrite_rewards.append(
+                example[reward_key][w_rewritten_rewrite_key]
+            )
 
     Y0_count = len(dataset) - Y1_count
 
@@ -216,38 +181,62 @@ def calculate_treatment_effects(
     logging.info(f"Number of w=1 examples: {Y1_count}")
     logging.info(f"Number of w=0 examples: {Y0_count}")
 
-    # TODO: Counting the Y1, Y0 examples above and calculating treatment effects separately is kind of confusing
-
-    # Calculate effect size with rewritten rewrites
-    treatment_effects = calculate_average_treatment_effects(
-        dataset,
-        rewritten_rewrites=True,
-        **effects_template,
+    # Naive effects
+    naive_effect, naive_effect_stderr = _calculate_treatment_effects(
+        Y1_rewards, Y0_rewards
     )
 
-    # Calculate effect size with only one rewrite
-    treatment_effects_naive = calculate_average_treatment_effects(
-        dataset,
-        rewritten_rewrites=False,
-        **effects_template,
+    # Single rewrite effects
+    ATE_single_rewrite, ATE_single_rewrite_stderr = _calculate_treatment_effects(
+        Y_do1_rewards, Y_do0_rewards, paired=True
+    )
+    # TODO: double check this
+    ATT_single_rewrite, ATT_single_rewrite_stderr = _calculate_treatment_effects(
+        Y1_rewards, Y0_rewritten_rewards, paired=True
+    )
+    ATU_single_rewrite, ATU_single_rewrite_stderr = _calculate_treatment_effects(
+        Y1_rewritten_rewards, Y0_rewards, paired=True
     )
 
-    for key, value in treatment_effects_naive.items():
-        treatment_effects[f"{key}_single_rewrite"] = value
+    # Rewritten rewrite effects
+    ATE_rewritten_rewrite, ATE_rewritten_rewrite_stderr = _calculate_treatment_effects(
+        Y_do1_rewritten_rewrite_rewards, Y_do0_rewritten_rewrite_rewards
+    )
+    ATT_rewritten_rewrite, ATT_rewritten_rewrite_stderr = _calculate_treatment_effects(
+        Y1_rewritten_rewrite_rewards, Y0_rewritten_rewards
+    )
+    ATU_rewritten_rewrite, ATU_rewritten_rewrite_stderr = _calculate_treatment_effects(
+        Y1_rewritten_rewards, Y0_rewritten_rewrite_rewards
+    )
+
+    effects = {
+        "naive_effect": naive_effect,
+        "naive_effect_stderr": naive_effect_stderr,
+        "ATE_single_rewrite": ATE_single_rewrite,
+        "ATE_single_rewrite_stderr": ATE_single_rewrite_stderr,
+        "ATT_single_rewrite": ATT_single_rewrite,
+        "ATT_single_rewrite_stderr": ATT_single_rewrite_stderr,
+        "ATU_single_rewrite": ATU_single_rewrite,
+        "ATU_single_rewrite_stderr": ATU_single_rewrite_stderr,
+        "ATE_rewritten_rewrite": ATE_rewritten_rewrite,
+        "ATE_rewritten_rewrite_stderr": ATE_rewritten_rewrite_stderr,
+        "ATT_rewritten_rewrite": ATT_rewritten_rewrite,
+        "ATT_rewritten_rewrite_stderr": ATT_rewritten_rewrite_stderr,
+        "ATU_rewritten_rewrite": ATU_rewritten_rewrite,
+        "ATU_rewritten_rewrite_stderr": ATU_rewritten_rewrite_stderr,
+        "reward_std": pooled_std,
+        "Y1_count": Y1_count,
+        "Y0_count": Y0_count,
+    }
 
     pp = pprint.PrettyPrinter(indent=4)
-    logging.info(f"Treatment effects: {pp.pformat(treatment_effects)}")
+    logging.info(f"Treatment effects: {pp.pformat(effects)}")
     logging.info(
-        f"Calculated ATE (from ATT and ATU): {(Y1_count * treatment_effects['ATT'] + Y0_count * treatment_effects['ATU'])/len(dataset)}"
+        f"Calculated ATE (from ATT and ATU): {(Y1_count * effects['ATT_rewritten_rewrite'] + Y0_count * effects['ATU_rewritten_rewrite'])/len(dataset)}"
     )
 
     rewrite_effects = calculate_rewrite_effect(dataset, **effects_template)
     logging.info(f"Rewrite effects: {pp.pformat(rewrite_effects)}")
-
-    effects = {**treatment_effects, **rewrite_effects}
-    effects["reward_std"] = pooled_std
-    effects["Y1_count"] = Y1_count
-    effects["Y0_count"] = Y0_count
     return effects
 
 
@@ -288,7 +277,8 @@ if __name__ == "__main__":
     dataset = load_dataset_from_json(dataset_path)
 
     logging.info("Dataset loaded. Calculating treatment effects...")
-    effects = calculate_treatment_effects(dataset, **effects_template)
+    # effects = calculate_treatment_effects(dataset, **effects_template)
+    effects = treatment_effects_pipeline(dataset, **effects_template)
 
     effects.update(effects_template)
 
